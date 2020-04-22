dbdfa4731d2d6d84babd0918bada96cb014b7595
AndyRezkiSeptiawan <rohanlily554@gmail.com>
2020-04-22 21:22:27 +0800

    Input dengan InputDialog

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane
+
+public class DataTypes{
+ public static void main(String[] args{
+  
+  String namaDepan = "Andy";
+  String namaTengah = "Rezki";
+  String namaBelakang = "Septiawan";
+  int usia = 21;
+  int targetTahunKuliah = 4;
+  double ipk = 3.89764512 ;
+  char nilaiAbjad ='A';
+  boolean tampan = true;
+  
+  System.out.println("Nama Depan : " + namaDepan);
+  System.out.println("Nama Tengah : " + namaTengah);
+  System.out.println("Nama Belakang  : " + namaBelakang);
+  System.out.println("usia : " + usia);
+  System.out.println("Target kuliah : " + targetTahunKuliah +" Tahun ");
+  System.out.println("IPK : " + ipk);
+  System.out.println("Nilai Praktikum : " + nilaiAbjad);
+  System.out.println("Tampan : " + tampan);
+ }
+}
